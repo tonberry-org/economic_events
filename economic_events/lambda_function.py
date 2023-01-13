c7406d2fe45d92d643946caab64154ead883187e
Matthew Tondreau <mmtondreau@gmail.com>
2023-01-13 10:09:53 -0800

    Add slack notices

--- economic_events/lambda_function.py
+++ economic_events/lambda_function.py
@@ -2,12 +2,22 @@ from typing import Any, Mapping
 from economic_events.economic_event_fetcher import EconomicEventFetcher
 from economic_events.economic_event_ingestor import EconomicEentIngestor
 from datetime import date
+from slack_bot_client.slack_client import SlackClient, SlackChannel
+
+
+def slack_processed(result: dict[date, list[dict[str, Any]]]) -> None:
+    message = "Econmoic Events Ingestion\n"
+    for key, value in result.items():
+        message += f"- {key.isoformat()}: {len(value)}"
+    SlackClient().send(SlackChannel.MONITORING, message)
 
 
 def lambda_handler(event: Mapping[str, Any], context: Mapping[str, Any]) -> str:
     ingestor = EconomicEentIngestor()
-    result = EconomicEventFetcher().fetch_days_to_process(date.today())
+    today = date.today()
+    result = EconomicEventFetcher().fetch_days_to_process(today)
     for _, event_data in result.items():
         ingestor.ingest(event_data=event_data)
 
+    slack_processed(result)
     return "OK"
